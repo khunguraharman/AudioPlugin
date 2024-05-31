a97c9beb22162b10536a3baf79d0d360d2c96d74
khunguraharman <khungura_harman@hotmail.com>
2024-05-30 19:24:32 -0700

    Added a struct to hold chain data

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -9,7 +9,14 @@
 #pragma once
 
 #include <JuceHeader.h>
+//create a struct to track all the chain settings
 
+struct ChainSettings
+{
+    float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 1.0f };
+    float lowCutFreq{ 0 }, highCutFreq{ 0 };
+    int lowCutSlope{ 0 }, highCutSlope{ 0 };
+};
 //==============================================================================
 /**
 */
