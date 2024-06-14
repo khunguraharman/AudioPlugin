fb8ba5a0a948bc8dcefc9173d93754d2bbf337c2
khunguraharman <khungura_harman@hotmail.com>
2024-06-13 20:17:36 -0700

    Added Sliders to GUI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,13 +9,21 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+
 //==============================================================================
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (600, 300);
+    //std::cout << getComps().size() << std::endl;
+
+    for (auto* comp : getComps())
+    {
+        addAndMakeVisible(comp);
+        //std::cout << "Found a comp" << std::endl;
+    }
+    setSize (600, 400);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -37,12 +45,40 @@ void AudioPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    //bounding box for the component
+    auto bounds = getLocalBounds();
+    //dedicate some space to a response area
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+    //dedicate space to low cut
+    auto lowCutArea = bounds.removeFromLeft(bounds.getWidth() * 0.33);
+    //only 66% of the original bounds remain after executing the above line
+    //dedicate space to high cut
+    //remember that this function updates bounds, so to allocate 33% of the original space to high cut, you want 50% of 66%
+    auto highCutArea = bounds.removeFromRight(bounds.getWidth() * 0.5);
+
+    ////set your sliders to be in the bounds calculated above
+    //lowCutFreqSlider.setBounds(lowCutArea);
+    //highCutFreqSlider.setBounds(highCutArea);
+    lowCutFreqSlider.setBounds(lowCutArea.removeFromTop(lowCutArea.getHeight()*0.5));
+    lowCutSlopeSlider.setBounds(lowCutArea);
+
+    highCutFreqSlider.setBounds(highCutArea.removeFromTop(highCutArea.getHeight()*0.5));
+    highCutSlopeSlider.setBounds(highCutArea);
+
+    //set the freq slider at the top of the centrel column
+    peakFreqSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.33));
+    //again you want 50% of 66% 
+    peakGainSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.5));
+    ////for the last 33%
+    peakQualitySlider.setBounds(bounds);
+
 }
 
 std::vector<juce::Component*> AudioPluginAudioProcessorEditor::getComps()
 {
     return
     {
-        &peakFreqSlider, &peakGainSlider, &peakQualitySlider,&lowCutFreqSlider, &highCutFreqSlider
+        &peakFreqSlider, &peakGainSlider, &peakQualitySlider, &lowCutFreqSlider, &highCutFreqSlider, &lowCutSlopeSlider, &highCutSlopeSlider
     };
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -17,7 +17,7 @@
 
 struct CustomRotarySlider : juce::Slider 
 {
-    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox);
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox) {};
 };
 class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
@@ -33,7 +33,7 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& audioProcessor;
-    CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider;
+    CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
 
     std::vector<juce::Component*> getComps();
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
